4bfe47c0c2eabb73e79cca110d943562769381a5
Krishna Neupane <kneupane@email.sc.edu>
2022-06-14 21:55:52 -0400

    Update clas12_yields.hpp

--- src/exe/clas12_yields.hpp
+++ src/exe/clas12_yields.hpp
@@ -186,7 +186,7 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<SyncFile>& _syn
     output.weight_gen = mc_event->weight();
 
     _sync->write(output);
-    }
+    // }
     // }
   }
   std::cout << "Percent = " << 100.0 * total / num_of_events << std::endl;
